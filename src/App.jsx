fc560592575ab63ca758052ee84bcc63b8359c11
Eriksen Lezama <eriksen.lezama@gmail.com>
2023-11-04 19:30:56 -0500

    first setup vite + header

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from './components/Header/Header.jsx'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <h1>Hi changes</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import './Header.css'
+
+
+function Header() {
+  return (
+    <header>
+      <div>
+        <span className='first'>Campus</span><span className='second'>Complaint</span>
+      </div>
+    </header>
+  )
+}
+
+export default Header
+
