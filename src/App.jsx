113ef5756242d6cf6ab19c2e6717ac864496c72f
Eriksen Lezama <eriksen.lezama@gmail.com>
2023-11-04 20:36:38 -0500

    Dummy authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
 import './App.css'
 import Header from './components/Header.jsx'
+import { users } from './database/users.js'
+import { useState } from 'react'
 import Login from './layouts/login'
 
 function App() {
 
-  return (
-    <>
-      <Header />
-      <Login />
-    </>
-  )
+    const [errorLogin, setErrorLogin] = useState(false)
+    const [userAuthenticated, setUserAuthenticated] = useState(false)
+
+    const handleLogin = (usernameRef, passwordRef) => {
+        const username = usernameRef.current.value
+        const password = passwordRef.current.value
+
+        const user = users.find(user => user.username === username && user.password === password)
+
+        if (user) {
+            setErrorLogin(false)
+            setUserAuthenticated(true)
+        } else {
+            setErrorLogin(true)
+        }
+    }
+
+    return (
+        <>
+            <Header />
+            {
+                userAuthenticated
+                    ? <h1>Authenticated</h1>
+                    : <Login login={handleLogin} errorLogin={errorLogin} />
+            }
+
+        </>
+    )
 }
 
 export default App
--- src/layouts/login.jsx
+++ src/layouts/login.jsx
@@ -1,12 +1,15 @@
 import { useRef } from 'react'
 import './login.css'
 
-function Login() {
+// eslint-disable-next-line react/prop-types
+function Login({ login, errorLogin }) {
 
     const usernameRef = useRef()
     const passwordRef = useRef()
 
-    const handleSubmit = () => {
+    const handleLogin = (e) => {
+        e.preventDefault()
+        login(usernameRef, passwordRef)
     }
 
     return (
@@ -17,7 +20,8 @@ function Login() {
                 <input ref={usernameRef} type="text" placeholder="Enter Username" />
                 <label>Password</label>
                 <input ref={passwordRef} type="password" placeholder="Enter Password" />
-                <button onClick={handleSubmit}>Login</button>
+                <button type='text' onClick={handleLogin}>Login</button>
+                <p className={errorLogin ? "error display" : "error"}>wrong password or username</p>
                 <p>Do not have an account? <a href="#">Register</a></p>
             </form>
         </div>
