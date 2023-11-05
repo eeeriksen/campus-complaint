1d363cd145e19ce0ec3dad0542907b7399437739
Justintelligent <justin.truong04@gmail.com>
2023-11-04 20:47:52 -0500

    Added posts database and 2 posts

---
+++ src/database/complaints.js
@@ -0,0 +1,46 @@
+export const posts = [
+  {
+    postId: 1,
+    username: "JohnDoe",
+    subject: "Giant Pothole in the Student Parking Lot",
+    content: "This pothole has been a nuisance for a whopping two weeks now. Everytime I drive through this parking lot I get back problems.",
+    created: "Feb. 05, 2023",
+    upvotes: 99,
+    comments: [
+      {
+        commentId: 1,
+        username: "JaneDoe",
+        content: "Relatable.",
+        created: "Feb. 05, 2023",
+      },
+      {
+        commentId: 2,
+        username: "JimmyDoe",
+        content: "A lof of people can seriously get injured from this.",
+        created: "Feb. 06, 2023",
+      }
+    ]
+  },
+  {
+    postId: 2,
+    username: "MonkeyDLuffy",
+    subject: "Urinal in Building A is unflushable",
+    content: "This urinal has been broken for the past three weeks and no one has done anything to fix it yet. In addition, it is the only urinal in that restroom.",
+    created: "Jan. 01, 2024",
+    upvotes: 2342,
+    comments: [
+      {
+        commentId: 1,
+        username: "Zoro",
+        content: "It's seriously a hassle when I have to wait to take a quick p### but someone is already in the stall.",
+        created: "Jan. 02, 2024",
+      },
+      {
+        commentId: 2,
+        username: "NamiSwan",
+        content: "My guy friend told me about this and it seriously sucks.",
+        created: "Jan. 04, 2024",
+      }
+    ]
+  }
+]
