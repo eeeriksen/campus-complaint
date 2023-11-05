5b57551f2c44b812d06b0c5e052a29af11a942d5
Eriksen Lezama <eriksen.lezama@gmail.com>
2023-11-05 09:36:54 -0600

    Router init

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,16 @@ import './App.css'
 import Header from './components/Header.jsx'
 import { users } from './database/users.js'
 import { useState } from 'react'
-import Login from './layouts/login'
-import Home from './layouts/home'
+import Login from './routes/login'
+import Home from './routes/home'
+import { createBrowserRouter, Router, RouterProvider } from 'react-router-dom'
 
 function App() {
+    const router = createBrowserRouter([
+        { path: '/', action: () => <Home /> },
+        { path: '/login', action: () => <Login /> },
+
+    ])
 
     const [errorLogin, setErrorLogin] = useState(false)
     const [userAuthenticated, setUserAuthenticated] = useState(false)
@@ -26,12 +32,15 @@ function App() {
 
     return (
         <>
-            <Header />
+            <RouterProvider router={router}>
+                <Router />
+            </RouterProvider>
+            {/* <Header />
             {
                 userAuthenticated
                     ? <Home />
                     : <Login login={handleLogin} errorLogin={errorLogin} />
-            }
+            } */}
 
         </>
     )
